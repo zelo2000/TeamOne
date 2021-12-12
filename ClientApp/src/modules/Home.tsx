a6f804f1defe7edf864aa9ca33d5d127df878c03
DenManokhin <denny1301@gmail.com>
2021-12-12 21:40:35 +0200

    Add trip card list for each trip status to the home page

--- ClientApp/src/modules/Home.tsx
+++ ClientApp/src/modules/Home.tsx
@@ -1,8 +1,11 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Typography } from 'antd';
 import TripService from '../services/TripService';
 import { TripModel } from '../models/TripModel';
+import { TripStatus } from '../models/TripStatus';
 import TripsCardList from "../components/TripsCardList";
+
+const { Title } = Typography;
 
 const Home: FC = () => {
   const [trips, setTrips] = useState([] as TripModel[]);
@@ -19,11 +22,26 @@ const Home: FC = () => {
   }, []);
   
   return (
-    <Row justify="center" className="trip-content">
-      <Col xs={24} sm={22} md={20} lg={18}>
-        <TripsCardList trips={trips}/>
-      </Col>
-    </Row>
+    <>
+      <Row justify="center" className="trip-content">
+        <Col xs={24} sm={22} md={20} lg={18}>
+          <Title>In progress</Title>
+          <TripsCardList trips={trips.filter(trip => trip.Status === TripStatus.InProgress)}/>
+        </Col>
+      </Row>
+        <Row justify="center" className="trip-content">
+        <Col xs={24} sm={22} md={20} lg={18}>
+          <Title>Planned</Title>
+          <TripsCardList trips={trips.filter(trip => trip.Status === TripStatus.Planned)}/>
+        </Col>
+      </Row>
+      <Row justify="center" className="trip-content">
+        <Col xs={24} sm={22} md={20} lg={18}>
+          <Title>Finished</Title>
+          <TripsCardList trips={trips.filter(trip => trip.Status === TripStatus.Closed)}/>
+        </Col>
+      </Row>
+    </>
   );
 };
 
