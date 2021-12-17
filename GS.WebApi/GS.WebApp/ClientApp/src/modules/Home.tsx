125ba8939ce0fc327a341d6b52c0051c231afe5b
Mykhailo <bordun.mykhailo24@gmail.com>
2021-12-17 14:32:15 +0200

    Updated Home page

--- GS.WebApi/GS.WebApp/ClientApp/src/modules/Home.tsx
+++ GS.WebApi/GS.WebApp/ClientApp/src/modules/Home.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { Col, Row } from "antd";
+import bg from "../assets/background.png";
 
 const Home: FC = () => {
   return (
-    <Row>
-      <Col>Go and See</Col>
+    <Row justify="center">
+      <Col span={24} className="welcome-label">{"Welcome to Go & See. Login to see more... "}</Col>
+      <Col span={24} className="bg-img"><img src={bg} alt="GoSee background"/></Col>
     </Row>
   );
 }
