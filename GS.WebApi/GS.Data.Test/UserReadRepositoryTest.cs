e822840b2e8580d5a337df385f4ae0d9b6e279dd
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2022-05-01 01:49:02 +0200

    Add user repositories test

---
+++ GS.WebApi/GS.Data.Test/UserReadRepositoryTest.cs
@@ -0,0 +1,88 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using GS.Data.Entities;
+using GS.Data.Repositories.UserRead;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GS.Data.Test
+{
+    public class UserReadRepositoryTest
+    {
+        private UserReadRepository _userReadRepository;
+        private GSDbContext _dbContext;
+        protected Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<GSDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test" + Guid.NewGuid())
+                .Options;
+
+            _dbContext = new GSDbContext(options);
+            _userReadRepository = new UserReadRepository(_dbContext);
+
+            _fixture = new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [Test]
+        public async Task GetUserByLoginAsync_WithExistingData_User()
+        {
+            var user = _fixture.Create<User>();
+            _dbContext.Add(user);
+            _dbContext.SaveChanges();
+
+            var userLogin = user.UserLogins.FirstOrDefault();
+
+            var userFromDb = await _userReadRepository.GetUserByLoginAsync(userLogin.LoginProvider, userLogin.ProviderKey);
+
+            userFromDb.Should().NotBeNull();
+            userFromDb.Id.Should().Be(user.Id);
+        }
+
+        [Test]
+        public async Task GetUserByLoginAsync_WithoutExistingLoginProvide_User()
+        {
+            var user = _fixture.Create<User>();
+            _dbContext.Add(user);
+            _dbContext.SaveChanges();
+
+            var userLogin = user.UserLogins.FirstOrDefault();
+
+            var userFromDb = await _userReadRepository.GetUserByLoginAsync(Guid.NewGuid().ToString(), userLogin.ProviderKey);
+
+            userFromDb.Should().BeNull();
+        }
+
+        [Test]
+        public async Task GetUserByLoginAsync_WithoutExistingProviderKey_User()
+        {
+            var user = _fixture.Create<User>();
+            _dbContext.Add(user);
+            _dbContext.SaveChanges();
+
+            var userLogin = user.UserLogins.FirstOrDefault();
+
+            var userFromDb = await _userReadRepository.GetUserByLoginAsync(userLogin.LoginProvider, Guid.NewGuid().ToString());
+
+            userFromDb.Should().BeNull();
+        }
+
+        [Test]
+        public async Task GetUserByLoginAsync_WithoutExistingData_User()
+        {
+            var user = _fixture.Create<User>();
+            _dbContext.Add(user);
+            _dbContext.SaveChanges();
+
+            var userFromDb = await _userReadRepository.GetUserByLoginAsync(Guid.NewGuid().ToString(), Guid.NewGuid().ToString());
+
+            userFromDb.Should().BeNull();
+        }
+    }
+}
---
+++ GS.WebApi/GS.Data.Test/UserWriteRepositoryTest.cs
@@ -0,0 +1,54 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using GS.Data.Entities;
+using GS.Data.Repositories.UserWrite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace GS.Data.Test
+{
+    public class UserWriteRepositoryTest
+    {
+        private UserWriteRepository _userWriteRepository;
+        private GSDbContext _dbContext;
+        protected Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<GSDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test" + Guid.NewGuid())
+                .Options;
+
+            _dbContext = new GSDbContext(options);
+            _userWriteRepository = new UserWriteRepository(_dbContext);
+
+            _fixture = new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [Test]
+        public async Task AddUser_ShoulAddUsers()
+        {
+            var user = _fixture.Create<User>();
+
+            await _userWriteRepository.AddUserAsync(user);
+
+            var userAfterAdd = await _dbContext.Users.ToListAsync();
+            userAfterAdd.Should().HaveCount(1);
+        }
+
+        [Test]
+        public async Task AddUser_ShoulAddUserLogins()
+        {
+            var userLogins = _fixture.Create<UserLogin>();
+
+            await _userWriteRepository.AddUserLoginAsync(userLogins);
+
+            var userAfterAdd = await _dbContext.UserLogins.ToListAsync();
+            userAfterAdd.Should().HaveCount(1);
+        }
+    }
+}
